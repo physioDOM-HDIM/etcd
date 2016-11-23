696c04989a341df1ba6911f7ba8b009ed5067e7c
fablec <fabrice.lecoz@telecomsante.com>
2016-11-23 16:39:48 +0100

    download the etcd binary

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+unset ETCD_VERSION
 etcd --listen-client-urls 'http://0.0.0.0:2379,http://0.0.0.0:4001' --advertise-client-urls 'http://0.0.0.0:2379,http://0.0.0.0:4001' --data-dir /data
